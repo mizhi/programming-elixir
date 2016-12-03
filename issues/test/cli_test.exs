e8522ecf09a3571437c0555158116c7815a29b9d
Mitch Peabody <mpeabody@panoramaed.com>
2016-12-02 19:39:33 -0500

    Chapter 13 issues project
    One difference in my implementation is that I compensate for the fact
    that my editor is set to automatically delete white space. There
    HEREDOC in table_formatter_test.exs was getting mangled. Instead of fixing my
    editor, I tweaked the code to trim spaces from the start and end of
    each formatted line.
    
    Bonus learning: :io_lib.format/2 will give you a character list. String.trim/2
    doesn't understand this, so it's necessary to call to_string/1 on the return
    from :io_lib.format/2 before passing to String.trim/2.

---
+++ issues/test/cli_test.exs
@@ -0,0 +1,22 @@
+defmodule CLliTest do
+  use ExUnit.Case
+  doctest Issues
+
+  import Issues.CLI, only: [parse_args: 1]
+
+  test ":help returned by --help" do
+    assert parse_args(["--help", "anything"]) == :help
+  end
+
+  test ":help returned by -h" do
+    assert parse_args(["-h", "anything"]) == :help
+  end
+
+  test "three values returned if three given" do
+    assert parse_args(["user", "project", "99"]) == { "user", "project", 99 }
+  end
+
+  test "count is defaulted if two values given" do
+    assert parse_args(["user", "project"]) == { "user", "project", 4 }
+  end
+end
